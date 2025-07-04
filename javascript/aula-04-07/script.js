e63e26abbb3ed77451098379933e3ba0100b3de2
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-04 13:32:32 -0300

    feat: else if

---
+++ javascript/aula-04-07/script.js
@@ -0,0 +1,13 @@
+// let idade = 20
+// if (idade >= 18)console.log("maior de idade")
+//     else console.log("menor de idade")
+
+// let idade = 10
+// if (idade >=60) console.log("Velho!")
+// else if (idade >= 17) console.log("jovem!") // else if para adicionar outras condições
+// else console.log("criança!")
+
+// let nota= 6
+// if (nota >= 7)console.log("Aprovado")
+//     else if (nota >= 5)console.log("Recuperação")
+//             else console.log("Reprovado")
