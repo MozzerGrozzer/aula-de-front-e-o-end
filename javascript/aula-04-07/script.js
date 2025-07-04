222459fee5919cf07d69f0cdd52f67d1a32cbe84
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-04 14:40:35 -0300

    feat: switch js

--- javascript/aula-04-07/script.js
+++ javascript/aula-04-07/script.js
@@ -11,3 +11,27 @@
 // if (nota >= 7)console.log("Aprovado")
 //     else if (nota >= 5)console.log("Recuperação")
 //             else console.log("Reprovado")
+
+// let numero = 2
+// // Compara valores fixos if e else em grandes famílias de números
+// switch(numero){
+//     case 1:console.log("número 1"); break
+//     case 2:console.log("Número 2"); break
+//     case 3:console.log("Número 3"); break
+//     case 4:console.log("Número 4"); break
+//     case 5:console.log("Número 5"); break
+//     default:console.log("número ausente")
+// }
+
+let dia = 4
+
+switch(dia){
+    case 1:console.log("Domingo");break
+    case 2:console.log("Segunda");break
+    case 3:console.log("Terça");break
+    case 4:console.log("Quarta");break
+    case 5:console.log("Quinta");break
+    case 6:console.log("Sexta");break
+    case 7:console.log("Sábado");break
+    default:console.log("insira número válido")
+}
