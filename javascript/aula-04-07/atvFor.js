a315d91082796d2ba14408dd8c8aa4f17b92cda1
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-04 16:29:35 -0300

    feat: atv For.js

---
+++ javascript/aula-04-07/atvFor.js
@@ -0,0 +1,21 @@
+for(let i=1; i<=10; i=i+1){
+    console.log(i)
+}
+
+for(let a=0; a<=20; a=a+2){
+    console.log(a)
+}
+
+for(let b=10; b>=1; b=b-1){
+    console.log(b)
+}
+
+for(let z=1; z<=10; z=z+1){
+    console.log(`5 x ${z} = ${5*z}`)
+}
+
+let total = 0
+for(let k=1; k<=100; k=k+1){
+    total+=k
+}
+console.log(total)
