c0ca645bb9b0c772cf8412864663efce28480c21
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-04 13:50:48 -0300

    feat: atv javascript

---
+++ javascript/aula-04-07/atvConditionalsTwo.js
@@ -0,0 +1,32 @@
+let nota = prompt
+if (nota >= 9)console.log("Excelente!!!")
+    else if (nota >= 7)console.log("Bom!")
+        else if (nota >= 5)console.log("regular")
+            else console.log("saia")
+
+let idade = prompt
+if (idade>=12)console.log("R$10")
+    else if(idade>=17)console.log("R$15")
+        else if(idade>=59)console.log("R$30")
+            else console.log("R$ 20")
+
+let peso=prompt
+if (peso>=60)console.log("catergoria leve")
+    else if (peso>=90)console.log("catergoria média")
+        else console.log("catergoria alta")
+
+let dia=prompt
+if (dia==1)console.log("Domingo")
+    else if (dia==2)console.log("Segunda")
+        else if (dia==3)console.log("Terça")
+            else if (dia==4)console.log("Quarta")
+                else if (dia==5)console.log("Quinta")
+                    else if (dia==6)console.log("Sextou!!!")
+                        else if (dia==7)console.log("Sábado")
+else console.log("insira número válido")
+
+let pagamento = prompt
+if(pagamento<=1.500)console.log("Sortudo")
+    else if(pagamento>=1.501||pagamento<=3.000)console.log("10%")
+        else if(pagamento>=3.000||pagamento<=5.000)console.log("15%")
+else console.log("20%")
