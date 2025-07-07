4fd1fcab768d43de3b32d925b368ede4cf3e437d
mozzergrozzer <grozzermozzer@gmail.com>
2025-07-07 14:34:23 -0300

    feat: function

--- javascript/aula-07-07/script.js
+++ javascript/aula-07-07/script.js
@@ -6,10 +6,36 @@
 //     i++
 // }
 
-let senha = "abc"
-let senhaDigitada = prompt("Coloque senha")
+// let senha = "abc"
+// let senhaDigitada = prompt("Coloque senha")
 
-while(senhaDigitada != senha){
-    alert("Senha incorreta")
-    senhaDigitada=prompt("Informe sua senha:")
+// while(senhaDigitada != senha){
+//     alert("Senha incorreta")
+//     senhaDigitada=prompt("Informe sua senha:")
+// }
+
+//funções resolvem problemas específicos
+// function nome (){}
+
+// function dizerOla (){
+//     console.log("Olá")
+// }
+
+// dizerOla()
+
+// function boarsVindas (){
+//     console.log("Seja bem-vindo")
+// }
+
+// boarsVindas()
+// função sem parametros parenteses vazias"()""
+// function fazerSuco(){console.log("fazendo suco de laranja!")}
+
+// fazerSuco()
+
+function fazerSuco(suco){
+    console.log(`fazendo suco de ${suco}`)
 }
+
+fazerSuco("Uva")
+fazerSuco("morango")
