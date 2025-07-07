6db3a80cc3821d75ce0897767e9307542860b7f3
mozzergrozzer <grozzermozzer@gmail.com>
2025-07-07 15:37:44 -0300

    feat: atv functions

---
+++ javascript/aula-07-07/atvFunction.js
@@ -0,0 +1,43 @@
+function helloWorld (){
+    console.log("hello world")
+}
+
+helloWorld()
+
+function mostrarCurso(){
+    console.log("Desevonvimento Web com JavaScript")
+}
+
+mostrarCurso()
+
+function mensagemMotivacional(){
+    console.log("do it!")
+}
+
+mensagemMotivacional()
+
+function dobro () {
+    console.log(`A multiplicação desse numero é: ${n1 * 2}`)
+}
+
+let n1 = 5
+
+dobro()
+
+function multiplicaFunction (){
+    console.log(`Multiplicação entre ${p1} e ${denonminador} é ${p1 * denonminador}`)
+}
+
+let p1 = 10
+let denonminador = 2
+
+multiplicaFunction()
+
+function alunoCredentials (){
+    console.log(`Aluno: ${nome} | Curso: ${curso}`)
+}
+
+let nome = "Garrett"
+let curso = "financia"
+
+alunoCredentials()
