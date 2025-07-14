0d6864a7e6f4eb3fbc808b21791049c943c38e6a
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-14 15:42:35 -0300

    feat: converter

---
+++ javascript/aula-11-07/atvClickEvent.js
@@ -0,0 +1,6 @@
+let textChange = document.getElementById('textChange')
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click', function(){
+    textChange.innerText = 'texto alterado com sucesso!'
+})
