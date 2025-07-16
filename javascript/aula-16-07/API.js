2613cc58a8e507abad23867cf5cb5477930e9f68
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-16 16:50:47 -0300

    feat: API done json

--- javascript/aula-16-07/API.js
+++ javascript/aula-16-07/API.js
@@ -5,5 +5,8 @@ let username = document.getElementById('username')
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((res) => res.json())
     .then((data) => {
-        console.log(data) // toda vez que trabalhar em API dar console.log(data) para encontrar objetos específicos
-    })
+        console.log(data[2]) // toda vez que trabalhar em API dar console.log(data) para encontrar objetos específicos
+        nome.innerText = data[2].name
+        email.innerText=data[2].email 
+        username.innerText=data[2].username 
+    },)
