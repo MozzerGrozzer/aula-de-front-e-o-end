276b6fa1bc1de8c683ce745f47fb9ef83d7c13d2
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-18 13:49:36 -0300

    feat: completed api

--- javascript/aula-18-07/api.js
+++ javascript/aula-18-07/api.js
@@ -1,11 +1,25 @@
 let nome = document.getElementById('nome')
 let email = document.getElementById('email')
 let username = document.getElementById('username')
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        nome.innerText=data[0].name
-        email.innerText=data[0].email
-        username.innerText=data[0].username
+let btn = document.getElementById('btn')
+let input = document.getElementById('input')
+
+const buscarUsuario=()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            nome.innerText=input.name
+            email.innerText=data[0].email
+            username.innerText=data[0].username
     })
+    .catch((err)=>{
+        nome.innerText = "quebrou"
+        email.innerText= "quebrou"
+        username.innerText= "quebrou"
+    })
+}
+
+btn.addEventListener('click',()=>{
+    buscarUsuario()
+})
