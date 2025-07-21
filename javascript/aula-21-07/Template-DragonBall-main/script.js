eea33633d3a98d57c0c35e60d5f04df278b1a606
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-21 16:54:24 -0300

    feat: dragonball api

--- javascript/aula-21-07/Template-DragonBall-main/script.js
+++ javascript/aula-21-07/Template-DragonBall-main/script.js
@@ -1,7 +1,27 @@
-let personagensContainer = document.getElementById('personagensContainer')
+let personagensContainer = document.getElementById('personagens-container')
 
-fetch('https://dragonball-api.com/api-docs')
+fetch('https://dragonball-api.com/api/characters')
     .then((res)=>res.json())
     .then((data)=>{
         console.log(data)
+        // console.log(data.items[0])
+        // console.log(data.items[0].name)
+        // console.log(data.items[0].gender)
+        // console.log(data.items[0].race)
+        // console.log(data.items[0].image)
+
+        data.items.forEach(element=> {
+            const card = document.createElement("div")
+            card.className="card"
+            card.innerHTML=`
+                <img src="${element.image}"/>
+                <h2>${element.name}</h2>
+                <p>raça: ${element.race}</p>
+                <p>gênero: ${element.gender}</p>
+            `
+            personagensContainer.appendChild(card)
+        })
+    })
+    .catch((err)=>{
+        //bleh
     })
